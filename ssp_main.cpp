05dcb509627028beb619be746d1afe89317ea339
AzHaR <azharakbar@gmail.com>
2015-05-15 08:10:11 +0530

    JUST ADDED THE EVALUATOR FN. TO THE CODE

--- ssp_main.cpp
+++ ssp_main.cpp
@@ -43,6 +43,7 @@ void show_data(char[] , char[]);
 void search_customer(char[] , float* val = 0 , int inv_no = 0);
 void search_product(char[] );
 int  scan(char [],char []);
+int evaluator ( char expr[] ) ;
 
 
 		//GLOBAL VARIABLES
@@ -86,7 +87,7 @@ public:
 	product()
 	{
 		strcpy (barcode , "0000") ;
-		strcpy(name,"");
+		strcpy (name , "" );
 		price=0.0;
 		stock = -1 ;
 	}
@@ -172,7 +173,14 @@ public :
 	}
 };
 
+struct stack 
+{
+	int operand ;
+	int opcode ;
+};
 
+stack A[100] ;
+int top = -1 ;
 
 
 
@@ -5413,3 +5421,65 @@ void bill::show_data(char type[] , int offset )
 
 }
 
+
+int evaluator ( char expr[] )
+{
+	int result = 0 , i = 0 , opcode = 0 , temp = 0 ;
+
+	while ( expr[i] != '\0' )
+	{
+		if ( expr[i] >= 48 && expr[i] <= 57 )
+		{
+			temp = ( temp *10 ) + ( (int)(expr[i]) - 48 ) ;
+		}
+
+		else if ( expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/' )
+		{
+			if ( !result )
+				result = temp ;
+			else
+			{
+				if ( opcode == 1 ) result += temp ;
+				else if ( opcode == 2 ) result -= temp ;
+				else if ( opcode == 3 ) result *= temp ;
+				else if ( opcode == 4 ) result /= temp ;
+			}
+
+			temp = 0 ;
+			if ( expr [i] == '+' ) opcode = 1 ;
+			else if ( expr [i] == '-' ) opcode = 2 ;
+			else if ( expr [i] == '*' ) opcode = 3 ;
+			else if ( expr [i] == '/' ) opcode = 4 ;
+		}
+
+		else if ( expr[i] == '(' )
+		{
+			if ( i && result )
+			{
+				A[++top].operand = result ;
+				A[top].opcode = opcode ;
+				result = temp = opcode = 0 ;
+			}
+		}
+
+		else if ( expr[i] == ')' )
+		{
+				if ( opcode == 1 ) result += temp ;
+				else if ( opcode == 2 ) result -= temp ;
+				else if ( opcode == 3 ) result = result * temp ;
+				else if ( opcode == 4 ) result /= temp ;
+				opcode = 0 ;
+			if ( top != -1 )
+			{
+				if ( A[top].opcode == 1 ) result += A[top--].operand ;
+				else if ( A[top].opcode == 2 ) result -= A[top--].operand ;
+				else if ( A[top].opcode == 3 ) result *= A[top--].operand ;
+				else if ( A[top].opcode == 4 ) result /= A[top--].operand ;
+			}
+			temp = 0 ;
+		}
+		++i ;
+	}
+
+	return result ;
+}
